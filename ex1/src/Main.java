e314df0c051b64fb2870111ec5bcb41a76ee4720
Boaz <Boazyakubov@gmail.com>
2022-11-18 01:38:46 +0200

    cleanup

--- ex1/src/Main.java
+++ ex1/src/Main.java
@@ -1,102 +1,76 @@
-   
+
 import java.io.*;
 import java.io.PrintWriter;
 
 import java_cup.runtime.Symbol;
-   
-public class Main
-{
-	static public void main(String[] argv)
-	{
-		/*define new lexer*/
-		Lexer currLexer;
-		Symbol currSymbol;
-		FileReader file_reader;
-		PrintWriter file_writer;
-		String inputFilename = argv[0];
-		String outputFilename = argv[1];
-		
-		try
-		{
-			/* [1] Initialize a file reader */
-			file_reader = new FileReader(inputFilename);
 
-			/* [2] Initialize a file writer */
-			file_writer = new PrintWriter(outputFilename);
+public class Main {
+    static public void main(String[] argv) {
+        /*define new lexer*/
+        Lexer currLexer;
+        Symbol currSymbol;
+        FileReader file_reader;
+        PrintWriter file_writer;
+        String inputFilename = argv[0];
+        String outputFilename = argv[1];
 
-			/* [3] Initialize a new lexer */
-			currLexer = new Lexer(file_reader);
+        try {
+            /* [1] Initialize a file reader */
+            file_reader = new FileReader(inputFilename);
 
-			/* [4] Read next token */
-			currSymbol = currLexer.next_token();
+            /* [2] Initialize a file writer */
+            file_writer = new PrintWriter(outputFilename);
 
-			/* [5] Main reading tokens loop */
-			while (currSymbol.sym != TokenNames.EOF)
-			{
-				/*if the symbol is error, just output error*/
-				if(currSymbol.sym == TokenNames.error)
-				{
-					file_writer.close();
-					file_writer = new PrintWriter(outputFilename);
-					System.out.println("ERROR");
-					file_writer.print("ERROR");
-					break;
-				}
+            /* [3] Initialize a new lexer */
+            currLexer = new Lexer(file_reader);
 
-				/* [6] Print to console -
-				 * construct a string like LPAREN[7,8] for line 7 char 8
-				 * or INT(74)[3,8] or STRING("Dan")[2,5] or ID(numPts)[1,6] */
+            /* [4] Read next token */
+            currSymbol = currLexer.next_token();
 
-				String tokenString = "";
-				tokenString += TokenNames.numToName(currSymbol.sym);
-				/*something in the braces, like INT(123), STRING("sup", ID(hey)*/
-				if(currSymbol.value!=null)
-				{
-					tokenString+= ("("+ currSymbol.value+")");
-				}
-				tokenString+="[";
-				tokenString+= currLexer.getLine();
-				tokenString+=",";
-				tokenString+= currLexer.getTokenStartPosition();
-				tokenString += "]\n";
-				System.out.print(tokenString);
-/*
+            /* [5] Main reading tokens loop */
+            while (currSymbol.sym != TokenNames.EOF) {
+                /*if the symbol is error, just output error*/
+                if (currSymbol.sym == TokenNames.error) {
+                    file_writer.close();
+                    file_writer = new PrintWriter(outputFilename);
+                    System.out.println("ERROR");
+                    file_writer.print("ERROR");
+                    break;
+                }
 
-				System.out.print("[");
-				System.out.print(currLexer.getLine());
-				System.out.print(",");
-				System.out.print(currLexer.getTokenStartPosition());
-				System.out.print("]:");
-				System.out.print(currSymbol.value);
-				System.out.print("\n");
-*/
+                /* [6] Print to console -
+                 * construct a string like LPAREN[7,8] for line 7 char 8
+                 * or INT(74)[3,8] or STRING("Dan")[2,5] or ID(numPts)[1,6] */
 
-				/* [7] Print to file */
+                String tokenString = "";
+                tokenString += TokenNames.numToName(currSymbol.sym);
+                /*something in the braces, like INT(123), STRING("sup", ID(hey)*/
+                if (currSymbol.value != null) {
+                    tokenString += ("(" + currSymbol.value + ")");
+                }
+                tokenString += "[";
+                tokenString += currLexer.getLine();
+                tokenString += ",";
+                tokenString += currLexer.getTokenStartPosition();
+                tokenString += "]\n";
+                System.out.print(tokenString);
 
-				file_writer.print(tokenString);
-/*
-				file_writer.print(currLexer.getLine());
-				file_writer.print(": ");
-				file_writer.print(currSymbol.value);
-				file_writer.print("\n");
-*/
+                /* [7] Print to file */
+                file_writer.print(tokenString);
 
-				/* [8] Read next token */
-				currSymbol = currLexer.next_token();
-			}
+                /* [8] Read next token */
+                currSymbol = currLexer.next_token();
+            }
 
-			/* [9] Close lexer input file */
-			currLexer.yyclose();
+            /* [9] Close lexer input file */
+            currLexer.yyclose();
 
-			/* [10] Close output file */
-			file_writer.close();
-    	}
-			     
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+            /* [10] Close output file */
+            file_writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
