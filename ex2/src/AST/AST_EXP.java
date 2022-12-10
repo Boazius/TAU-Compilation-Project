f1bf3d915cc84d1973735594274a7668b233b93f
Boaz <Boazyakubov@gmail.com>
2022-12-10 05:31:04 +0200

    Semicolon!!!!!!!!!!!!!!!

--- ex2/src/AST/AST_EXP.java
+++ ex2/src/AST/AST_EXP.java
@@ -1,8 +1,6 @@
 package AST;
 
 public abstract class AST_EXP extends AST_Node {
-    public int moish;
-
     public void PrintMe() {
         System.out.print("UNKNOWN AST DECELERATION NODE");
     }
